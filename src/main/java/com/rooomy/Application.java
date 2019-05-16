ae964b687a97a25181bb62e0745d652b9698e1b9
Lin Xia <lin.xia@nuance.com>
2019-05-16 11:12:49 +0800

    add licence check

--- src/main/java/com/rooomy/Application.java
+++ src/main/java/com/rooomy/Application.java
@@ -3,23 +3,32 @@ package com.rooomy;
 import com.rooomy.gui.MainPage;
 
 import javax.swing.*;
+import java.util.Calendar;
+import java.util.Date;
 
-public class Application extends JFrame{
+public class Application extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public Application() {
-		super("CLTFConverter");
-		this.getContentPane().add(new MainPage());
-		
-		this.setSize(620,650);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);	
-		this.setResizable(false);
-	}
-	
-	public static void main(String[] args) {
-		new Application();
-	}
+    public Application() {
+        super("CLTFConverter");
+        if (validLicence()) {
+            this.getContentPane().add(new MainPage());
+            this.setSize(620, 650);
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            this.setLocationRelativeTo(null);
+            this.setVisible(true);
+            this.setResizable(false);
+        }
+    }
+
+    private boolean validLicence() {
+        Date today = new Date();
+        Date endDay = new Date(2020 - 1900, 1, 1);
+        return today.before(endDay);
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
 }
